bc308a768068f5b1c8a8074cc96d935b40b08e92
user159552 <user159552@92-63-141-223.rdns.melbourne.co.uk>
2019-10-27 12:26:12 +0000

    WeatherApp init and gitignore

---
+++ WeatherApp/WeatherApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by user159552 on 10/27/19.
+//  Copyright © 2019 user159552. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
