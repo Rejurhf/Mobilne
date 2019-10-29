4c495190656ee318ac9e560592127aaf6d4952fb
user159552 <user159552@92-63-141-223.rdns.melbourne.co.uk>
2019-10-29 18:30:45 +0000

    UI recreated, bindings added, buttons dummy set up

--- Take3/Take3/ViewController.swift
+++ Take3/Take3/ViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: UIvariables
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var stateLabel: UILabel!
+    @IBOutlet weak var minTempLabel: UILabel!
+    @IBOutlet weak var maxTempLabel: UILabel!
+    @IBOutlet weak var windSpeedLabel: UILabel!
+    @IBOutlet weak var windDirectionLabel: UILabel!
+    @IBOutlet weak var airPressureLabel: UILabel!
+    @IBOutlet weak var humidityLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // Mark: Buttons action
+    @IBAction func previousButtonOnClick(_ sender: UIButton) {
+        locationLabel.text = "Previosu"
+        dateLabel.text = "Previosu"
+        stateLabel.text = "Previosu"
+        minTempLabel.text = "Previosu"
+        maxTempLabel.text = "Previosu"
+        windSpeedLabel.text = "Previosu"
+        windDirectionLabel.text = "Previosu"
+        airPressureLabel.text = "Previosu"
+        humidityLabel.text = "Previosu"
+    }
+    
+    @IBAction func nextButtonOnClick(_ sender: UIButton) {
+        locationLabel.text = "Next"
+        dateLabel.text = "Next"
+        stateLabel.text = "Next"
+        minTempLabel.text = "Next"
+        maxTempLabel.text = "Next"
+        windSpeedLabel.text = "Next"
+        windDirectionLabel.text = "Next"
+        airPressureLabel.text = "Next"
+        humidityLabel.text = "Next"
+    }
+    
 }
 
