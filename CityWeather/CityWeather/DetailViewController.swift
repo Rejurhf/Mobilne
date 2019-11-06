40078cb18f30910340ceff38503720d0a3510190
user159552 <user159552@92-63-141-223.rdns.melbourne.co.uk>
2019-11-06 08:35:47 +0000

    Detail view done

--- CityWeather/CityWeather/DetailViewController.swift
+++ CityWeather/CityWeather/DetailViewController.swift
@@ -19,12 +19,25 @@ class DetailViewController: UIViewController {
     @IBOutlet weak var humidityLabel: UILabel!
     @IBOutlet weak var airPresureLabel: UILabel!
     
+    @IBOutlet weak var previousButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    
     @IBOutlet weak var stateImage: UIImageView!
+    
+    let DISPLAYABLE_DAYS = 7
+    
+    var locationName: String! = ""
+    var woeid: Int! = 0
+    var displayedDate: Date! = Date()
+    var displayedDayNumber = 0
     
     func configureView() {
         // Update the user interface for the detail item.
         if let detail = detailItem {
-            
+            displayedDate = detail.date!
+            locationName = detail.cityName
+            woeid = Int(detail.cityWOEID)
+            updateView(weatherReport: detail)
         }
     }
 
@@ -32,6 +45,7 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         configureView()
+        locationLabel.text = locationName
     }
 
     var detailItem: City? {
@@ -43,10 +57,56 @@ class DetailViewController: UIViewController {
 
 
     @IBAction func onClickPreviousButton(_ sender: UIButton) {
+        nextButton.isEnabled = true
+        displayedDayNumber -= 1
+        displayedDate = Calendar.current.date(byAdding: .day, value: -1, to: displayedDate)
+        createWeatherReport(locationName: locationName, woeid: woeid, date: displayedDate, callback: { (weatherReport) in
+            self.updateView(weatherReport: weatherReport)
+        })
+        if displayedDayNumber <= 0 {
+            previousButton.isEnabled = false
+        }
     }
     
     
     @IBAction func onClickNextButton(_ sender: UIButton) {
+        previousButton.isEnabled = true
+        displayedDayNumber += 1
+        displayedDate = Calendar.current.date(byAdding: .day, value: 1, to: displayedDate)
+        createWeatherReport(locationName: locationName, woeid: woeid, date: displayedDate, callback: { (weatherReport) in
+            self.updateView(weatherReport: weatherReport)
+        })
+        if displayedDayNumber >= DISPLAYABLE_DAYS - 1 {
+            nextButton.isEnabled = false
+        }
+    }
+    
+    func updateView(weatherReport: City) {
+        if let label = dateLabel {
+            let dateComponents = Calendar.current.dateComponents([.year, .month, .day], from: weatherReport.date!)
+            label.text = "\(String(describing: dateComponents.day!))/\(String(describing: dateComponents.month!))/\(String(describing: dateComponents.year!))"
+        }
+        if let label = temperatureLabel {
+            label.text = "\(String(describing: weatherReport.temperature!)) °C"
+        }
+        if let label = humidityLabel {
+            label.text = "\(String(describing: weatherReport.humidity!))%"
+        }
+        if let label = windSpeedLabel {
+            label.text = "\(String(describing: weatherReport.windSpeed!)) mph"
+        }
+        if let label = windDirectionLabel {
+            label.text = "\(String(describing: weatherReport.windDirection!))"
+        }
+        if let label = airPresureLabel {
+            label.text = "\(String(describing: weatherReport.airPressure!)) mbar"
+        }
+        if let view = stateImage {
+            view.image = weatherReport.weatherStateIcon!
+        }
+        if let label = weatherLabel {
+            label.text = "\(String(describing: weatherReport.weatherStateAbbreviation!))"
+        }
     }
 }
 
